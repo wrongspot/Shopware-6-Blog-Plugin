c30d7b1b96d9e87e3d287e94b8090661e51a3425
Christopher Dosin <hello@christopherdosin.me>
2020-12-15 20:52:36 +0200

    Removed editor.js

---
+++ src/Resources/app/administration/build/webpack.config.js
@@ -0,0 +1,13 @@
+const { join, resolve } = require('path');
+
+module.exports = () => {
+    return {
+        resolve: {
+            alias: {
+                '@slugify': resolve(
+                    join(__dirname, '..', 'node_modules', 'slugify')
+                )
+            }
+        }
+    };
+}
